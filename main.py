1bf2085342647c7aeba670a7ad1388a35180f270
LooTcFeaR <76649939+LooTcFeaR@users.noreply.github.com>
2021-03-19 11:58:40 +0100

    Implemented environment varbiable for app secret.

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from functools import wraps
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from flask import Flask, render_template, redirect, url_for, flash, abort
-from flask_bootstrap import Bootstrap
-from flask_ckeditor import CKEditor
-from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
+from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
+from dotenv import load_dotenv
+from functools import wraps
+from datetime import date
+import os
+
+load_dotenv("D:/Python/Environment_Variables/.env")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
